d2a0666b0fb47a0efcf0dc0f583023b043a31c1a
anastasiia-kolieva <nastya1110@gmail.com>
2021-02-15 19:24:30 +0200

    added dirname, handleError, Commander

--- contacts.js
+++ contacts.js
@@ -1,13 +1,22 @@
-import path from 'path';
-import fileSystem from 'fs';
-const fs = fileSystem.promises;
+// Так работает!!!!!!
+// import path from 'path';
+// import fileSystem from 'fs';
+// const fs = fileSystem.promises;
 // Если не передается сегментов пути, path.resolve() возвращает абсолютный путь к текущей рабочей директории.
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 // console.log(__filename)
 
-const contactsPath = path.join(__dirname, 'db/contacts.json');
-console.log(__dirname)
-console.log(contactsPath)
+// const contactsPath = path.join(__dirname, '/db/contacts.json');
+// console.log(__dirname)
+// console.log(contactsPath)
+
+// выдаёт ошибку: Cannot find package 'fs' imported from D:\GO IT\NODE\homeworks\1\Node.js-homework\contacts.js
+// у меня версия ноды v12.18.3
+import * as fs from "fs/promises";
+import * as path from "path";
+
+import getDirname from './helpers/dirname.js'
+// const { __dirname } = getDirname(import.meta.url);
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
