4c7a41c9168a476cac66be5a247370c8dce883c2
anastasiia-kolieva <nastya1110@gmail.com>
2021-02-16 18:43:53 +0200

    added function getContactById

--- contacts.js
+++ contacts.js
@@ -7,24 +7,54 @@ const { __dirname } = getDirname(import.meta.url);
 
 const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-// TODO: задокументировать каждую функцию
 export async function listContacts() {
   try {
+    // данные с прочитки файла contacts.json
     const data = await fs.readFile(contactsPath);
-    console.log(data.toString());
+
+    // Проверить что приходит в консоле!!!!
+
+    // JSON.parse необходимо чтоб распарсить в обьект, потому что там строка сформированна
+    console.log(JSON.parse(data.toString()));
   } catch (error) {
     handleError(error);
   }
 }
 
-export function getContactById(contactId) {
-  // ...твой код
+export async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    // JSON.parse необходимо чтоб распарсить в обьект, потому что там строка сформированна
+    // данные с прочитки файла contacts.json
+    const parseData = JSON.parse(data.toString());
+    // Проверить!!!!!
+    console.log(data.toString())
+    console.log(parseData)
+
+    //в данных с прочитки файла contacts.json ищем необходимый контакт по id
+    const contact = parseData.find((contact) => contact.id === contactId);
+    if (!contact) console.error("Contact not found.");
+    console.log(contact);
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-export function removeContact(contactId) {
-  // ...твой код
+export async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parseData = JSON.parse(data.toString());
+
+    const filteredContacts = parseData.filter(
+      (contact) => contact.id !== contactId
+    );
+
+
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-export function addContact(name, email, phone) {
+export async function addContact(name, email, phone) {
   // ...твой код
 }
